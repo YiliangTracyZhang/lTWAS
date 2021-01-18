23ef38e4756342faec77b14fbec5520cda7d18d6
YiliangTracyZhang <yiliang.zhang@yale.edu>
2021-01-17 23:28:05 -0500

    update

--- calculate.py
+++ calculate.py
@@ -57,18 +57,14 @@ def calculate(bfile, gwas_snps, N1, N2, h1, h2):
     tz2 = np.dot(sub_v.T, gwas_snps['Z_y'])
     y = tz1 * tz2
 
-    Z_x = gwas_snps['Z_x']
-    Z_y = gwas_snps['Z_y']
-
-    
     threshold = 1
     cur_d = sub_d[sub_d>threshold]
     cur_y = y[sub_d>threshold]
     cur_dsq = cur_d ** 2
-    denominator = (wh1 * cur_d / m0 + 1 / n1) * (wh2 * cur_d / m0 + 1 / n2)
+    denominator = (h1 * cur_d / m + 1 / N1) * (h2 * cur_d / m + 1 / N2)
     cur_v1 = np.sum(cur_dsq / denominator)
-    cur_v2 = np.sum(cur_y / sqrt(n1 * n2) / denominator)
-    cur_v3 = np.sum(cur_y ** 2 / (n1 * n2) / (denominator * cur_dsq))
+    cur_v2 = np.sum(cur_y / sqrt(N1 * N2) / denominator)
+    cur_v3 = np.sum(cur_y ** 2 / (N1 * N2) / (denominator * cur_dsq))
 
     emp_var = [(cur_v3 - (cur_v2 ** 2) / cur_v1) / (cur_v1 * (len(cur_d) - 1))]
     theo_var = [1 / cur_v1]
@@ -76,9 +72,9 @@ def calculate(bfile, gwas_snps, N1, N2, h1, h2):
     for K in range(len(cur_d), len(sub_d)):
         eig = sub_d[K]
         tmp_y = y[K]
-        cur_v1 += eig ** 2 / ((wh1 * eig / m0 + 1 / n1) * (wh2 * eig / m0 + 1 / n2))
-        cur_v2 += tmp_y / sqrt(n1 * n2) / ((wh1 * eig / m0 + 1 / n1) * (wh2 * eig / m0 + 1 / n2))
-        cur_v3 += tmp_y ** 2 / (n1 * n2) / ((wh1 * eig ** 2 / m0 + eig / n1) * (wh2 * eig ** 2 / m0 + eig / n2))
+        cur_v1 += eig ** 2 / ((h1 * eig / m + 1 / N1) * (h2 * eig / m + 1 / N2))
+        cur_v2 += tmp_y / sqrt(N1 * N2) / ((h1 * eig / m + 1 / N1) * (h2 * eig / m + 1 / N2))
+        cur_v3 += tmp_y ** 2 / (N1 * N2) / ((h1 * eig ** 2 / m + eig / N1) * (h2 * eig ** 2 / m + eig / N2))
         emp_var.append((cur_v3 - (cur_v2 ** 2) / cur_v1) / (cur_v1 * K))
         theo_var.append(1 / cur_v1)
     
@@ -88,47 +84,15 @@ def calculate(bfile, gwas_snps, N1, N2, h1, h2):
     y = y[:(len(cur_d)+min_idx-1)]
     sub_d = sub_d[:(len(cur_d)+min_idx-1)]
     sub_dsq = sub_d ** 2
-
-    var_rho = m0 ** 2 * min(max_emp_theo)
-    q = (wh1 * sub_d / m0 + 1 / n1) * (wh2 * sub_d / m0 + 1 / n2)
-    v4 = np.sum(sub_d/q)/np.sum(sub_dsq/q)
-    var_phencorr = pheno_corr_var / (n1 * n2) * m0 ** 2 * v4 ** 2
-    var_rho += var_phencorr
-
+    q = (h1 * sub_d / m + 1 / N1) * (h2 * sub_d / m + 1 / N2)
+    var_rho = m ** 2 * min(max_emp_theo)
+    rho = m / sqrt(N1 * N2) * (np.sum(y / q)) / (np.sum(sub_dsq / q))
     se_rho = sqrt(var_rho)
-    p_value = norm.sf(abs(Localrho / se_rho)) * 2
+    p_value = norm.sf(abs(rho / se_rho)) * 2
 
-    if Localh1 < 0 or Localh2 < 0:
-        corr = np.nan
-    else:
-        corr = Localrho / sqrt(Localh1 * Localh2)
+    corr = rho / sqrt(h1 * h2)
 
-    df = pd.DataFrame(OrderedDict({"chr":[CHR], "start":[START], "end":[END], "rho":[Localrho], "corr":[corr], "h2_1":[Localh1], "h2_2":[Localh2], "var":[var_rho], "p":[p_value], "m":[m0]}))
-
-    return df
-
-def _supergnova(bfile, partition, thread, gwas_snps, ld_scores, n1, n2, pheno_corr, pheno_corr_var):
-
-    geno_array = array_obj(array_file, n, array_snps, keep_snps=keep_snps,
-        keep_indivs=keep_indivs, mafMin=None)
-    coords = np.array(array_snps.df['CM'])[geno_array.kept_snps]
-    bps = np.array(array_snps.df['BP'])[geno_array.kept_snps]
-
-    ## Calculating local genetic covariance
-    
-    results = []
-    def collect_results(result):
-        results.append(result)
-    pool = multiprocessing.Pool(processes = thread)
-    for i in range(blockN):
-        pool.apply_async(calLocalCov, args=(i, tmp_partition, geno_array, coords, 
-            bps, tmp_gwas_snps, tmp_ld_scores, n1, n2, pheno_corr, pheno_corr_var),
-            callback=collect_results)
-    pool.close()
-    pool.join()
-    df = pd.concat(results, ignore_index=True)
-    #df = pd.DataFrame(results)
-    #df.columns = ["chr", "start", "end", "rho", "corr", "h1", "h2", "var", "p", "m"]
-    convert_dict = {"chr": int, "start": int, "end":int, "m":int}
+    df = pd.DataFrame(OrderedDict({"rho":[rho], "corr":[corr], "h2_1":[h1], "h2_2":[h2], "var":[var_rho], "p":[p_value], "m":[m]}))
+    convert_dict = {"m":int}
     df = df.astype(convert_dict)
     return df
